1eeebd191ebdd96f4f594e5c044714993061fda0
Thariq Aulia Akbar <thariq2712@gmail.com>
2026-01-08 14:03:14 +0700

    first commit

---
+++ src/main/java/com/dibimbing/apiassignment/ApiAssignmentDay27Application.java
@@ -0,0 +1,13 @@
+package com.dibimbing.apiassignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiAssignmentDay27Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiAssignmentDay27Application.class, args);
+    }
+
+}
