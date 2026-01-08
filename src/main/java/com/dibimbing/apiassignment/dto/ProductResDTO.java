a1b2730d3ac90ab5c6555e04093f8d1bc6d58728
Thariq Aulia Akbar <thariq2712@gmail.com>
2026-01-09 01:00:14 +0700

    feat/add product response dto

---
+++ src/main/java/com/dibimbing/apiassignment/dto/ProductResDTO.java
@@ -0,0 +1,15 @@
+package com.dibimbing.apiassignment.dto;
+
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class ProductResDTO {
+    private Long id;
+    private String name;
+    private String description;
+    private Double price;
+    private Integer quantity;
+}
