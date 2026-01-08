86be8da2d1a8d1807bc5983b33ec80ba90d19447
Thariq Aulia Akbar <thariq2712@gmail.com>
2026-01-09 01:00:49 +0700

    feat/add product controller

---
+++ src/main/java/com/dibimbing/apiassignment/controller/ProductController.java
@@ -0,0 +1,48 @@
+package com.dibimbing.apiassignment.controller;
+
+import com.dibimbing.apiassignment.dto.ProductReqDTO;
+import com.dibimbing.apiassignment.dto.ProductResDTO;
+import com.dibimbing.apiassignment.service.ProductService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/products")
+public class ProductController {
+    private final ProductService productService;
+
+    @PostMapping
+    public void postProduct(@Valid @RequestBody ProductReqDTO request) {
+        productService.addProduct(request);
+    }
+
+    @GetMapping("/{id}")
+    public ProductResDTO getProductById(@PathVariable("id") Long id) {
+        return productService.getProductById(id);
+    }
+
+    @GetMapping
+    public List<ProductResDTO> getProducts() {
+        return productService.getProduct();
+    }
+
+    @PatchMapping("/{id}")
+    public void addProductStock(@PathVariable("id") Long id, @RequestBody Integer quantity) {
+        productService.addProductStock(id, quantity);
+    }
+
+    @PutMapping("/{id}")
+    public void updateProduct(@PathVariable("id") Long id, @RequestBody ProductReqDTO request) {
+        productService.updateProduct(id, request);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteProduct(@PathVariable("id") Long id) {
+        productService.deleteProduct(id);
+    }
+
+}
