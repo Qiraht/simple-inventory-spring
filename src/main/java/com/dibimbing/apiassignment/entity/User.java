50bdb7e11acd3267ae042ebd7f5bad7d68df673a
Thariq Aulia Akbar <thariq2712@gmail.com>
2026-01-10 22:04:21 +0700

    feat/User entity

---
+++ src/main/java/com/dibimbing/apiassignment/entity/User.java
@@ -0,0 +1,24 @@
+package com.dibimbing.apiassignment.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.*;
+import lombok.experimental.Accessors;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Accessors(chain = true)
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String username;
+    private String password;
+    private String email;
+    private String authority;
+}
