6372cfce82e676a919690023a4f368ec76800bc9
Thariq Aulia Akbar <thariq2712@gmail.com>
2026-01-10 22:02:17 +0700

    feat/add jwt service

---
+++ src/main/java/com/dibimbing/apiassignment/service/JwtService.java
@@ -0,0 +1,66 @@
+package com.dibimbing.apiassignment.service;
+
+import com.dibimbing.apiassignment.entity.User;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.stereotype.Service;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+import java.util.function.Function;
+
+@Service
+public class JwtService {
+    private final String SECRET_KEY = "30f4670c3737642dd1495a6dcc774cc376a72ebbcecec40c2dab2d097b515260b776ff986466eb20dee151692b98e2f76f7de462301a7411ac27128d1dc27ea0";
+
+    public String extractUsername(String token) {
+        try {
+            return extractClaim(token, Claims::getSubject);
+        } catch (Exception e) {
+            return null;
+        }
+    }
+
+    public <T> T extractClaim(String token, Function<Claims, T> resolver) {
+        final Claims claims = extractAllClaims(token);
+        return resolver.apply(claims);
+    }
+
+    private Claims extractAllClaims(String token) {
+        return Jwts.parserBuilder()
+                .setSigningKey(SECRET_KEY.getBytes())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+    public String generateToken(User user) {
+        Map<String, Object> claims = new HashMap<>();
+        claims.put("roles", user.getAuthority());
+        claims.put("jti", UUID.randomUUID().toString());
+
+        return Jwts.builder()
+                .setClaims(claims)
+                .setIssuer("auth-service")
+                .setSubject(user.getUsername())
+                .setAudience("my-frontend-app")
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + 5 * 60 * 1000)) // 5 min
+                .signWith(Keys.hmacShaKeyFor(SECRET_KEY.getBytes()), SignatureAlgorithm.HS256)
+                .compact();
+
+    }
+
+    public boolean isTokenValid(String token, User user) {
+        final String username = extractUsername(token);
+        return username.equals(user.getUsername()) && !isExpired(token);
+    }
+
+    private boolean isExpired(String token) {
+        return extractClaim(token, Claims::getExpiration).before(new Date());
+    }
+}
