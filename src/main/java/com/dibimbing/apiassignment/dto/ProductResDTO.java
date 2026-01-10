8e04ede5818ed37033c6dbec81ffc40f8f389c4c
Thariq Aulia Akbar <thariq2712@gmail.com>
2026-01-11 02:22:53 +0700

    feat/implements serialize for redis cache

--- src/main/java/com/dibimbing/apiassignment/dto/ProductResDTO.java
+++ src/main/java/com/dibimbing/apiassignment/dto/ProductResDTO.java
@@ -2,11 +2,17 @@ package com.dibimbing.apiassignment.dto;
 
 import lombok.*;
 
+import java.io.Serial;
+import java.io.Serializable;
+
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
-public class ProductResDTO {
+public class ProductResDTO implements Serializable {
+    @Serial
+    private static final long serialVersionUID = 1L;
+
     private Long id;
     private String name;
     private String description;
