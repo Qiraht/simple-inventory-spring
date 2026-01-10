ef77dcbc381f65f673e439da7383e7f274544283
Thariq Aulia Akbar <thariq2712@gmail.com>
2026-01-11 01:36:38 +0700

    feat/fix controller return

--- src/main/java/com/dibimbing/apiassignment/controller/UserController.java
+++ src/main/java/com/dibimbing/apiassignment/controller/UserController.java
@@ -6,6 +6,7 @@ import com.dibimbing.apiassignment.dto.UserRegisterReqDTO;
 import com.dibimbing.apiassignment.service.UserService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,12 +19,17 @@ public class UserController {
     private final UserService userService;
 
     @PostMapping("/register")
-    public String postRegister(@Valid @RequestBody UserRegisterReqDTO request) {
-        return userService.registerUser(request);
+    public ResponseEntity<String> postRegister(
+            @Valid @RequestBody UserRegisterReqDTO request) {
+        String result = userService.registerUser(request);
+
+        return ResponseEntity.ok(result);
     }
 
     @PostMapping("/login")
-    public UserLoginResDTO postLogin(@Valid @RequestBody UserLoginReqDTO request) {
-        return userService.LoginUser(request);
+    public ResponseEntity<UserLoginResDTO> postLogin(@Valid @RequestBody UserLoginReqDTO request) {
+        UserLoginResDTO result = userService.LoginUser(request);
+
+        return ResponseEntity.ok(result);
     }
 }
