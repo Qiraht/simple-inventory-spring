7cc3cc014c525d9fe5208d007bc0806348e3dc3d
Thariq Aulia Akbar <thariq2712@gmail.com>
2026-02-11 01:14:26 +0700

    feat/init test

---
+++ src/test/java/com/dibimbing/apiassignment/ApiAssignmentDay27ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dibimbing.apiassignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiAssignmentDay27ApplicationTests extends BaseIntegrationTest {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
